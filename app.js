8b5f093ccf79ae6d8a5ca0296accf728e100eaef
eyo.omat@gmail.com <eyo.omat@gmail.com>
2017-01-17 12:38:16 +0100

    home screen development

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+var app = express();
+
+var routes = require('./routes');
+
+app.set('view engine'. 'ejs');
+
+var path require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', routes.home);
+
+app.listen(4400, function(){
+	console.log("The application runs on localhost:4400");
+})
+
+
+
+
+
+
+
+
+
+
