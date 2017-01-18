8243f26cb2702500c4293efabc35ca47cd8f899b
eyo.omat@gmail.com <eyo.omat@gmail.com>
2017-01-18 10:01:50 +0100

    User interface showing images from firebase tables

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express();
 
 var routes = require('./routes');
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true })); 
 app.set('view engine', 'ejs');
 
 var path = require('path');
@@ -11,6 +13,10 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', routes.home);
+app.get('/addbooks', routes.addbooks);
+app.post('/addbook', routes.addbook);
+app.get('/signup', routes.signup);
+app.get('/booksummary/:bookname?', routes.singlebook);
 
 app.listen(4400, function(){
 	console.log("The application runs on localhost:4400");
--- routes/index.js
+++ routes/index.js
@@ -14,31 +14,95 @@ var firebase = require('firebase');
 // Home Route
 exports.home = function(req, res) {
 
-	var books = firebase.database().ref('books/');
-	var users = firebase.database().ref('Users/');
+	var books = firebase.database().ref('books');
+	var users = firebase.database().ref('Users');
 	var ref = firebase.database().ref();
 	
-	ref.on("value", function(snapshot) {
-	   //console.log(snapshot.val());
-	}, function (error) {
-	   console.log("Error: " + error.code);
-	});
-	console.log(users.toString());
+	// ref.on("value", function(snapshot) {
+	//    console.log(snapshot.val());
+	// }, function (error) {
+	//    console.log("Error: " + error.code);
+	// });
+
+	books.on("value", function(snapshot) {
+		var bookList  = snapshot.val();
 	res.render('home', {
 		title: "Library Valley",
-		books : books
+		books : bookList
 	});
+	console.log("Finished")
+	});
+	//console.log(userlist);
 
 	
 
 };
 
+exports.addbooks = function(req, res) {
+var books = firebase.database().ref('books');
 
+	books.on("value", function(snapshot) {
+		var bookList  = snapshot.val();
+	res.render('addbooks', {
+		title: "Add a Book",
+		books : bookList,
+	});
+	console.log("Finished")
+	});
+
+	/*$('.addValue').on("click", function( event ) {  
+    event.preventDefault();
+    //if( auth != null ){
+      if( $('#bookTitle').val() != '' || $('#author').val() != '' ){
+        books
+          .push({
+            bookName: $('#bookTitle').val(),
+            author: $('#author').val(),
+            year: $('#year').val(),
+            category: $('#category').val()
+          })
+          document.addBookForm.reset();
+      } else {
+        alert('Please fill atlease name or email!');
+      }
+    //} else {
+      //inform user to login
+    //}
+  });*/
+console.log(req);
+
+};
+
+exports.signup = function(req, res) {
+	var users = firebase.database().ref('Users');
+	res.render('signup', {
+		title: "Sign Up",
+	});
+	console.log("Finished")
+
+};
+
+exports.singlebook = function(req, res) {
+	var books = firebase.database().ref('books');
+
+	books.on("value", function(snapshot) {
+		var bookList  = snapshot.val();
+		var book = bookList[req.params.bookname];
+		console.log(book);
+	res.render('singlebook', {
+		title: book.bookName,
+		books : bookList,
+		book : book,
+	});
+	console.log("Finished")
+	});
+
+};
 // Route for all other page requests
 exports.notFound = function(req, res) {
 	var books = firebase.database().ref('books');
 	res.render('notFound', {
 		books : books,
-		title : "Oops, this page doesn't exist"
+		title : "Oops , this page doesn't exist"
 	});
 };
